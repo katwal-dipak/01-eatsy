1b3a5c5188f2171703c7f46ad3eca6a010bf3d98
Dipak Katwal <katwal.dipak@hotmail.com>
2021-05-16 15:28:22 +0545

    Initial commit

---
+++ eatsy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  eatsy
+//
+//  Created by Deepak Katuwal on 16/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ eatsy/eatsyApp.swift
@@ -0,0 +1,17 @@
+//
+//  eatsyApp.swift
+//  eatsy
+//
+//  Created by Deepak Katuwal on 16/05/21.
+//
+
+import SwiftUI
+
+@main
+struct eatsyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
